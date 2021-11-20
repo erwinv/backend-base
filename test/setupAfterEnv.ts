453514b891a8076b76fa96a653e40bddefbdc2ca
Erwin Villejo <erwin.villejo@gmail.com>
2021-11-20 20:03:59 +0700

    run knex migration on tests

--- test/setupAfterEnv.ts
+++ test/setupAfterEnv.ts
@@ -1,36 +1,44 @@
 import mongoose from 'mongoose'
 import objection from 'objection'
-import knex from 'knex'
+import Knex, { Knex as KnexT } from 'knex'
 import { redis } from '../lib/database/'
 
 jest.setTimeout(2000)
 jest.mock('ioredis', () => require('ioredis-mock/jest'))
 
-let connection: ReturnType<typeof knex>
+let knex: ReturnType<typeof Knex>
+
+const knexConfig: KnexT.Config = {
+  client: 'sqlite3',
+  connection: ':memory:',
+  useNullAsDefault: true,
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: 'migrations/',
+    loadExtensions: ['.js'],
+  },
+}
 
 beforeAll(async () => {
-  connection = knex({
-    client: 'sqlite3',
-    connection: ':memory:',
-    useNullAsDefault: true,
-  })
-
   await Promise.all([
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     mongoose.connect(process.env.MONGODB_URI!),
-    (async () => {
-      // TODO run migrations here
-    })(),
     redis.setup(),
+    (async () => {
+      knex = Knex(knexConfig)
+      await knex.migrate.latest()
+      objection.Model.knex(knex)
+    })(),
   ])
-
-  objection.Model.knex(connection)
 })
 
 afterAll(async () => {
   await Promise.all([
     mongoose.disconnect(),
-    connection.destroy(),
     redis.teardown(),
+    (async () => {
+      await knex.migrate.rollback()
+      await knex.destroy()
+    })(),
   ])
 })
